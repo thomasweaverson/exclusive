518c13bbe1d21f9aad0ad2359ff042e8223c2995
thomas <thomasweaverson@gmail.com>
2023-12-01 12:15:51 +0300

    added dots 4 banner slider

--- scripts/slickInit.js
+++ scripts/slickInit.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
   // слайдер баннер
   $('.banner_slick-slider').slick({
+    arrows : false,
     autoplay: true,
-    autoplaySpeed: 3001,
+    autoplaySpeed: 3000,
     infinite: true,
+    dots: true,
+    customPaging : function(slider, i) {
+      return '<a href="#"><img src="../img/banner_slider/banner_slider_dot.svg" /><img src="../img/banner_slider/banner_slider_dot_active.svg" /></a>';
+  },
   })
   //слайдер в секции Today's
   $('.today__slick-carousel').slick({
